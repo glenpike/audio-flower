81aaba9c423915341724d0e47ca79c9404e7777d
Glen Pike <github@glenpike.co.uk>
2018-08-25 00:25:34 +0100

    Control triggering audio with serial port.
    Hookup the Arduino capacitive sensor to this and read the input.
    Convert the input buffer to 'messages' and parse the sensor
    value from each one.
    If the sensor value drops below a threshold, then passes through a
    higher one, we trigger some audio.
    Still needs some kind of calibration.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,70 @@
 const Speaker = require('speaker');
 const fs = require('fs');
 
-const speaker = new Speaker({
-  channels: 2,
-  bitDepth: 16,
-  sampleRate: 44100,
+const debug = true;
+
+const triggerAudio = () => {
+  const speaker = new Speaker({
+    channels: 2,
+    bitDepth: 16,
+    sampleRate: 44100,
+  });
+  const audioFile = 'audio/chris/wanted-a-job.wav';
+
+  const audioSrc = fs.createReadStream(audioFile);
+  audioSrc.pipe(speaker);
+};
+
+const portName = '/dev/ttyACM0'
+const SerialPort = require('serialport');
+const port = new SerialPort(portName, {
+    baudRate: 9600,
+  },
+  function (err) {
+  if (err) {
+    return console.log('Error: ', err.message);
+  }
 });
 
-const audioFile = 'audio/chris/wanted-a-job.wav';
+// Need to smooth data over time, as we are triggering a lot.
+// two triggerThresholds - trigger & reset?
+const triggerThreshold = 800;
+const resetThreshold = 100;
+let triggered = false;
+const handleData = ({ time, value }) => {
+  const sensorValue = Number.parseInt(value);
+  if (Number.isNaN(sensorValue)) {
+    console.log('handleData - invalid sensor value: ', value);
+    return;
+  }
+  if (debug) {
+    console.log(`sensorValue: ${sensorValue} (${value})`);
+  }
+  if (sensorValue > triggerThreshold && !triggered) {
+    console.log('triggering...');
+    triggerAudio();
+    triggered = true;
+  } else if(sensorValue < resetThreshold && triggered) {
+    console.log('resetting trigger...');
+    triggered = false;
+  }
+};
 
-const audioSrc = fs.createReadStream(audioFile);
-audioSrc.pipe(speaker);
+let buffer = '';
+port.on('readable', function () {
+  buffer += port.read().toString();
+  const messages = buffer.split('\n');
+  if (messages.length > 1) {
+    // save the bit for the buffer.
+    buffer = messages.pop();
+    messages.forEach((m) => {
+      const regex = /([0-9]+)\t([0-9]+)/;
+      const match = m.match(regex);
+      // console.log('match ', match);
+      if (match) {
+        const [, time, value] = match;
+        handleData({ time, value});
+      }
+    });
+  }
+});
